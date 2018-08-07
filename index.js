3e8e7194c78ec1229e6aafde8c8d55f3ef78e782
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2018-08-07 12:15:57 +0800

    Coding finished
    Ready to use

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ var io = socket(server);
 
 io.on('connection',function(socket){
   console.log('socket connection established',socket.id)
+
+  socket.on("chat",function(data){
+    io.sockets.emit('chat',data);
+  });
+
+  socket.on('typing',function(data){
+    socket.broadcast.emit('typing',data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,31 @@
 // connection
 var socket = io.connect("http://localhost:5500");
+
+//DOM
+var message = document.getElementById('message');
+var username = document.getElementById('username');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var typingmessage = document.getElementById('typingmessage');
+
+// Emit
+btn.addEventListener('click',function(){
+  socket.emit('chat',{
+    message:message.value,
+    username:username.value
+  });
+  message.value = "";
+});
+
+message.addEventListener('keypress',function(){
+  socket.emit('typing',username.value);
+});
+//listen for events
+socket.on('chat',function(data){
+  typingmessage.innerHTML ="";
+  output.innerHTML +='<p><strong>' +data.username+':</strong>'+data.message+'</p>';
+});
+
+socket.on('typing',function(data){
+  typingmessage.innerHTML = '<p><em>'+data+'is typing a message...</em></p>';
+});
